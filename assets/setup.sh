fcfe2605b8158b308603d457c9e6bd2997a8ce70
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-01-02 15:01:17 +0800

    fix

--- assets/setup.sh
+++ assets/setup.sh
@@ -6,7 +6,7 @@ trap "echo_red '******* Caught SIGINT signal. Stopping...'; exit 2" SIGINT
 
 #Install prerequisites directly without virtual package
 echo "Installing dependencies"
-rm /etc/yum.repos.d/*.repo && cp /asserts/CentOS-Base.repo /etc/yum.repos.d
+rm /etc/yum.repos.d/*.repo && cp /assets/CentOS-Base.repo /etc/yum.repos.d
 yum makecache
 
 yum -y install binutils compat-libstdc++-33 compat-libstdc++-33.i686 ksh \
@@ -18,19 +18,19 @@ yum -y install binutils compat-libstdc++-33 compat-libstdc++-33.i686 ksh \
 yum clean all
 rm -rf /var/lib/{cache,log} /var/log/lastlog
 
+echo "Creating directories"
+mkdir -p -m 755 /u01/app/oracle
+mkdir -p -m 755 /u01/app/oraInventory
+mkdir -p -m 755 /u01/app/dpdump
+
 echo "Configuring users"
 groupadd -g 200 oinstall
 groupadd -g 201 dba
 groupadd -g 202 oper
 useradd -u 440 -g oinstall -G dba,oper -d /u01/app/oracle oracle
-
 echo "oracle:install" | chpasswd
 echo "root:install" | chpasswd
 sed -i "s/pam_namespace.so/pam_namespace.so\nsession    required     pam_limits.so/g" /etc/pam.d/login
-
-mkdir -p -m 755 /u01/app/oracle
-mkdir -p -m 755 /u01/app/oraInventory
-mkdir -p -m 755 /u01/app/dpdump
 chown -R oracle:oinstall /u01/app
 
 cat /assets/profile >> ~oracle/.bash_profile
