1d44960ace25c55e397ade0d42b4b3bd2d64a4bd
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-01-02 17:37:18 +0800

    update 2

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 source /assets/colorecho
 
-mount -o remount,size=4G /dev/shm
+# mount -o remount,size=4G /dev/shm
 
 if [ ! -d "/u01/app/oracle/product/11.2.0/dbhome_1" ]; then
   echo_yellow "Database is not installed. Installing..."
