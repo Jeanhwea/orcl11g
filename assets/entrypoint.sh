926549a1ed16763e5efc7f589516ae276f8401ec
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-01-02 17:57:45 +0800

    update

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -3,7 +3,9 @@
 set -e
 source /assets/colorecho
 
-mount -o remount,size=4G /dev/shm
+SHM_SIZE=4g
+echo "tmpfs /dev/shm tmpfs defaults,size=$SHM_SIZE 0 0" >> /etc/fstab
+mount -o remount,size=$SHM_SIZE /dev/shm
 
 if [ ! -d "/u01/app/oracle/product/11.2.0/dbhome_1" ]; then
   echo_yellow "Database is not installed. Installing..."
