1ac4e294c1cee7eb42ef7657d35f792137e02c9b
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-01-02 14:14:31 +0800

    init

---
+++ assets/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+source /assets/colorecho
+
+if [ ! -d "/u01/app/oracle/product/11.2.0/dbhome_1" ]; then
+  echo_yellow "Database is not installed. Installing..."
+  /assets/install.sh
+fi
+
+su oracle -c "/assets/entrypoint_oracle.sh"
