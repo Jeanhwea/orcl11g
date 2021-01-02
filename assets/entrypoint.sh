054363a82d811469815ad5b3e7f1a3a594973067
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-01-02 20:14:05 +0800

    setup dpdump folder

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -7,6 +7,9 @@ SHM_SIZE=4g
 echo "tmpfs /dev/shm tmpfs defaults,size=$SHM_SIZE 0 0" >> /etc/fstab
 mount -o remount,size=$SHM_SIZE /dev/shm
 
+chown oracle:oinstall /u01/app/dpdump
+chmod 777 /u01/app/dpdump
+
 if [ ! -d "/u01/app/oracle/product/11.2.0/dbhome_1" ]; then
   echo_yellow "Database is not installed. Installing..."
   /assets/install.sh
